8722dd9369c5df6a2f61f05d0897e4702d402e54
Arh33is <3ian9v@gmail.com>
2025-02-22 00:47:18 -0500

    F: Subiendo el server funcional, Api con solo Get a /users -> me trae el dato que necesito para el Front Mdv-react-ts-subs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import {Client} from 'pg'
+import { Request, Response } from 'express';
+
+// OBJETIVE
+[
+    {
+      nick: 'Carmen',
+      subMonths: 3,
+      avatar: 'https://i.pravatar.cc/150?u=dapelu',
+      description: 'Daleu hacce moredador a veces deleniti eius qui Eligendi nam suscipit ut dolorem molestiae eum. Neque vero adipisci culpa. Voluptas provident animi velit nulla in modi. Inventore labore ipsam. Voluptates quia ab earum.'
+    },
+    {
+      nick:'sergio_serrano',
+      subMonths: 7,
+      avatar:'https://i.pravatar.cc/150?u=sergio_serrano',
+    }
+  ]
+
 
 const app = express();
 
@@ -9,11 +27,44 @@ const app = express();
 app.use(morgan('dev'))
 app.use (cors())
 
+const client = new Client({
+    host: "localhost",
+    database: "mdv_react-ts-subs",
+    user: "postgres",
+    password: "root",
+  });
+
+const PORT:number =6505
+
+app.use(express.json());
+
+client
+  .connect()
+  .then(() => console.log("Conectado a la base de datos 👽"))
+  .catch((err:any) => console.error("Error al conectar a la base de datos", err));
+
+
 app.get( '/', (req,res)=>{
     console.log('Hola app.get');
     res.send('Hola mundo')
 })
 
-app.listen( 6505, ()=>{
-    console.log('Server activo 👽');
+
+app.get('/users', async (req:Request, res:Response)=>{
+
+    let query = `SELECT * FROM users;` 
+
+    try{
+        const result = await client.query(query)
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).send("Error en el servidor"); // Manejo de errores
+
+    }
+})
+
+
+app.listen( PORT, ()=>{
+    console.log('Server activo 👽',`http://localhost:6505`);
 }) 
